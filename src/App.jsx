98f1a7d657d3e00d839a622070245df4788b1548
Clean-Pick <benjamin.will98@gmail.com>
2025-02-20 14:04:57 +0100

    Header done
    Frontpage ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
+import Header from "./components/header.jsx";
 
 function App() {
 
     return (
-        <div className="w-screen h-screen flex justify-center items-center bg-green-900 text-white">
 
-            <h1 className="text-5xl font-bold underline uppercase">ceci n'est pas un test</h1>
+        <section className="font-movies-lato w-screen h-screen bg-moviesBg">
 
-        </div>
+            <Header/>
+
+            <section className="w-screen h-screen flex justify-center">
+
+                <div
+                    className="flex container w-full h-[200px] mx-[30px] p-4 bg-white rounded-[30px] shadow-lg ">
+
+                    <div className="relative bottom-0 container w-2/3 h-4/10 bg-gray-300 rounded-[20px]"></div>
+
+                </div>
+
+            </section>
+        </section>
     )
 }
 
---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+
+    return (
+
+        <section className="w-screen flex py-[32px]">
+
+            <div className="container mx-[30px] text-3xl">
+                <h2>
+                    <span className="text-orange-500">Movie</span>
+                    Browser
+                </h2>
+            </div>
+
+
+        </section>
+    )
+
+}
