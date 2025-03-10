29ee2a84991eff25af79aa0b28049d25a6885786
Clean-Pick <benjamin.will98@gmail.com>
2025-03-10 15:13:28 +0100

    Refactoring and reorganization
    TODO : movieDetails.jsx redirection + Discover searchbar + Discover categories

--- src/api/movieList.jsx
+++ src/api/movieList.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import apiClient from "./apiClient.jsx";
-import {motion} from "motion/react";
+import MovieCard from "../components/discover/movieCard.jsx";
 
 const MovieList = () => {
     const [movies, setMovies] = useState([]);
@@ -65,68 +65,22 @@ const MovieList = () => {
     return (
         <section className="mx-10 xl:mx-50">
             <h1>Movie List</h1>
-            <ul className="container flex flex-wrap gap-[20px] xl:gap-5 mt-[24px]  justify-between">
-                {movies.map((movie, index) => {
-                    const posterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
 
-                    return (
+            <ul className="container flex flex-wrap gap-[20px] xl:gap-5 mt-[24px] justify-between">
+                
+                {movies.map((movie, index) => (
 
-                        <motion.li
-                            key={movie.id}
-                            className="flex justify-center relative
-                                w-39 md:w-53 xl:w-70
-                                h-50 md:h-80 xl:h-100
-                                bg-white rounded-[30px]
-                                mb-[30px] lg:mb-[60px] xl:mb-[100px]"
-                            initial={{
-                                scale: 0,
-                                opacity: 0,
-                                zIndex: 1
-                            }}
-                            animate={{
-                                scale: 1,
-                                opacity: 1,
-                                x: 0,
-                                transition: {
-                                    type: "spring",
-                                    stiffness: 300,
-                                    damping: 20,
-                                    delay: index * 0.05,
-                                },
+                    <MovieCard
+                        key={movie.id}
+                        movie={movie}
+                        index={index}
+                    />
 
-                            }}
-                            exit={{opacity: 1}}
-                            whileTap={{scale: 0.9}}
-                            whileHover={{
-                                scale: 1.1,
-                                boxShadow: "0px 5px 5px #000",
-                                zIndex: 10
-                            }}
-                            style={{
-                                backgroundImage: `url(${posterUrl})`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center',
-                            }}
-                        >
-
-                            <div
-                                className="container flex justify-center items-center absolute
-                                    bottom-[-42px] md:bottom-[-60px] xl:bottom-[-80px]
-                                    w-10/10 h-2/10"
-                            >
-                                <p className="capitalize truncate">{movie.title} ({movie.release_date.substring(0, 4)})</p>
-                            </div>
-
-
-                        </motion.li>
-
-
-                    );
-                })}
+                ))}
             </ul>
             {loading && <div className="text-2xl">Loading more movies...</div>}
         </section>
-    )
+    );
 };
 
 
