18fa486cbc93b5c9da0f03bef777c9bc760eea76
Clean-Pick <benjamin.will98@gmail.com>
2025-03-17 12:46:05 +0100

    Add search functionality and error boundary to movie discovery page

--- src/pages/discover.jsx
+++ src/pages/discover.jsx
@@ -1,9 +1,47 @@
+// React/Movies/src/pages/discover.jsx
 import MovieList from "../api/movieList.jsx";
+import {useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
 
 export default function Discover() {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
     return (
-        <div className="mb-50 bg-moviesBg">
-            <MovieList/>
+        <div className="mb-50 mx-10 xl:mx-50 bg-moviesBg">
+
+            <div className="relative flex items-center mb-[16px]">
+
+                <FontAwesomeIcon
+                    icon={faSearch}
+                    className="
+                        text-gray-400
+                        absolute
+                        left-5"
+                />
+
+                <input
+                    type="text"
+                    placeholder="Search a movie..."
+                    value={searchQuery}
+                    onChange={handleSearchChange}
+                    className="
+                        search-bar
+                        flex
+                        w-full
+                        h-10
+                        rounded-[15px]
+                        bg-[#211f30]
+                        p-5
+                        pl-12"
+                />
+            </div>
+            
+            <MovieList searchQuery={searchQuery}/>
         </div>
-    )
+    );
 }
