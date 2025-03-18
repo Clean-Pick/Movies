381e8fbc5b74f0b1da77cd4cca8e5ea3cf320c23
Clean-Pick <benjamin.will98@gmail.com>
2025-03-18 14:25:56 +0100

    Add navigation link to header for homepage redirection

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,19 @@
+import {NavLink} from "react-router-dom";
+
 export default function Header() {
 
     return (
 
         <section className="w-screen flex py-[32px]">
 
-            <div className="container mx-10 xl:mx-50 text-3xl">
+            <NavLink to={"/"} className="container mx-10 xl:mx-50 text-3xl">
+
                 <h2>
                     <span className="text-orange-500">Movie</span>
                     Seeker
                 </h2>
-            </div>
 
+            </NavLink>
 
         </section>
     )
