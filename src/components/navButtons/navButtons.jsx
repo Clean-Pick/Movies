00c8d4be0e9f89170aeac723d659bd321e4aafe2
Clean-Pick <benjamin.will98@gmail.com>
2025-03-23 03:58:02 +0100

    refactor: replace SVG icons with FontAwesome icons in navigation buttons

--- src/components/navButtons/navButtons.jsx
+++ src/components/navButtons/navButtons.jsx
@@ -1,34 +1,44 @@
 import {NavLink} from "react-router-dom";
 import {motion} from "motion/react";
-import Home from "../../../public/icons/Home.svg";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHouse, faUser} from "@fortawesome/free-solid-svg-icons";
 import Discover from "../../../public/icons/Search.svg";
-import Profile from "../../../public/icons/Profile.svg";
 
 export default function NavButtons() {
     return (
         <ul className="container mx-10 xl:mx-50 flex justify-around">
             <li>
-                <NavLink to="/"
-                         className="flex justify-center items-center"
-                >
-                    <motion.img
-                        src={Home}
-                        alt="Home Icon"
-                        className="cursor-pointer h-10 lg:h-8 w-10 lg:w-8"
-                        initial={{scale: 1}}
+                <NavLink to="/" className="flex justify-center items-center">
+                    {/* Home */}
+                    <motion.div
+                        className="flex bg-[#3d3c45] cursor-pointer h-10 lg:h-8 w-10 lg:w-8 rounded-4xl justify-center items-center"
+                        initial={{
+                            scale: 1,
+                            backgroundImage: "",
+                            color: "#7d7c82"
+                        }}
                         whileHover={{
                             scale: 1.2,
-                            fill: "#124568"
+                            backgroundImage: "linear-gradient(240deg, rgba(243,136,108,1) 0%, rgba(175,74,59,1) 100%)",
+                            color: "rgba(255, 255, 255, 1)"
                         }}
                         whileTap={{scale: 0.9}}
-                    />
+                        transition={{
+                            backgroundImage: {duration: 0.3, ease: "easeInOut"},
+                            color: {duration: 0.3, ease: "easeInOut"}
+                        }}
+                    >
+                        <FontAwesomeIcon
+                            icon={faHouse}
+                            className="cursor-pointer h-10 lg:h-8 w-10 lg:w-8 text-current"
+                        />
+                    </motion.div>
                 </NavLink>
             </li>
 
             <li>
-                <NavLink to="/discover"
-                         className="flex justify-center items-center"
-                >
+                <NavLink to="/discover" className="flex justify-center items-center">
+                    {/* Discover */}
                     <motion.div
                         className="flex bg-[#3d3c45] cursor-pointer h-10 lg:h-8 w-10 lg:w-8 rounded-4xl justify-center items-center"
                         initial={{
@@ -40,8 +50,10 @@ export default function NavButtons() {
                             backgroundImage: "linear-gradient(240deg, rgba(243,136,108,1) 0%, rgba(175,74,59,1) 100%)"
                         }}
                         whileTap={{scale: 0.9}}
+                        transition={{
+                            backgroundImage: {duration: 0.3, ease: "easeInOut"}
+                        }}
                     >
-
                         <motion.img
                             src={Discover}
                             alt="Discover Icon"
@@ -50,24 +62,38 @@ export default function NavButtons() {
                             whileHover={{scale: 1.2}}
                             whileTap={{scale: 0.9}}
                         />
-
                     </motion.div>
-
                 </NavLink>
             </li>
 
             <li>
-                <NavLink to="/profile">
-                    <motion.img
-                        src={Profile}
-                        alt="Profile Icon"
-                        className="cursor-pointer h-10 lg:h-8 w-10 lg:w-8"
-                        initial={{scale: 1}}
-                        whileHover={{scale: 1.2}}
+                <NavLink to="/profile" className="flex justify-center items-center">
+                    {/* Profile */}
+                    <motion.div
+                        className="flex bg-[#3d3c45] cursor-pointer h-10 lg:h-8 w-10 lg:w-8 rounded-4xl justify-center items-center"
+                        initial={{
+                            scale: 1,
+                            backgroundImage: "",
+                            color: "#7d7c82"
+                        }}
+                        whileHover={{
+                            scale: 1.2,
+                            backgroundImage: "linear-gradient(240deg, rgba(243,136,108,1) 0%, rgba(175,74,59,1) 100%)",
+                            color: "rgba(255, 255, 255, 1)"
+                        }}
                         whileTap={{scale: 0.9}}
-                    />
+                        transition={{
+                            backgroundImage: {duration: 0.3, ease: "easeInOut"},
+                            color: {duration: 0.3, ease: "easeInOut"}
+                        }}
+                    >
+                        <FontAwesomeIcon
+                            icon={faUser}
+                            className="cursor-pointer h-10 lg:h-8 w-10 lg:w-8 text-current"
+                        />
+                    </motion.div>
                 </NavLink>
             </li>
         </ul>
-    )
+    );
 }
